cfae901cf5b8e3c7f6acced9b87e479c986e4cd9
Yann Pineill <yann@parallelstudio.tv>
2025-05-16 18:49:53 +0200

    Revert video sync

--- intergrations/sanity/slices/custom-column-media-grid/index.tsx
+++ intergrations/sanity/slices/custom-column-media-grid/index.tsx
@@ -44,6 +44,7 @@ export const CustomColumnMediaGrid: FC<CustomColumnMediaGridProps> = ({
 }) => {
   const containerRef = useRef<HTMLDivElement>(null)
 
+  /*
   // AJOUT YANN
     const videoRefs = useRef<HTMLVideoElement[]>([])
     videoRefs.current = [] // Reset refs on every render
@@ -91,7 +92,7 @@ export const CustomColumnMediaGrid: FC<CustomColumnMediaGridProps> = ({
       }
     }, [media])
   // FIN AJOUT YANN  
-
+*/
 
   const getMuxPlaceholderTimestamp = (media: IMedia) => {
     return  media.muxPlaceholderTimestamp
@@ -125,7 +126,7 @@ export const CustomColumnMediaGrid: FC<CustomColumnMediaGridProps> = ({
             />
           ) : (
             <MuxPlayer
-              ref={handleMuxRef} // AJOUT YANN !
+              // ref={handleMuxRef} // AJOUT YANN !
               className={cn(s.sliderItem__muxPlayer, 'mobile:w-screen', {
                 [s.autoplayMode]: media.autoplay,
               })}
